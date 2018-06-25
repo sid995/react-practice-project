dbd000b386cb0cc0b5c816f41eac960e8a698994
Siddharth Kundu <sk31395@gmail.com>
2018-06-25 23:32:58 +0530

    Side Project Using React

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
-import { Route } from 'react-router-dom';
+import { Route, NavLink } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
-import NewPost from './NewPost/NewPost';  
+import NewPost from './NewPost/NewPost'; 
+import FullPost from './FullPost/FullPost';
 
 class Blog extends Component {
     render () {
@@ -13,8 +14,22 @@ class Blog extends Component {
                 <header>
                     <nav>
                         <ul>
-                            <li><a href="/">Home</a></li>
-                            <li><a href="/new-post">New Post</a></li>
+                            <li><NavLink 
+                                to="/" 
+                                exact 
+                                activeClassName="active"
+                                activeStyle={{
+                                    color: '#fa923f',
+                                    textDecoration: 'underline'
+                                }}>Home</NavLink></li>
+                            <li><NavLink to={{
+                                // Relative Path
+                                // pathname: this.props.match.url +  '/new-post',
+                                // Absolute Path
+                                pathname: '/new-post',
+                                hash: '#submit',
+                                search: '?quick-submit=true'
+                            }}>New Post</NavLink></li>
                         </ul>
                     </nav>
                 </header>
@@ -22,6 +37,7 @@ class Blog extends Component {
                 <Route path = "/" render={() => <h1>Home2</h1>} /> */}
                 <Route path="/" exact component={Posts} />
                 <Route path="/new-post" component={NewPost} />
+                <Route path="/:id" exact component={FullPost} />
             </div>
         );
     }
