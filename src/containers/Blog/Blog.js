c35fde6af749fbe7bfa4fb60ee03a4811f557a85
Siddharth Kundu <sk31395@gmail.com>
2018-07-01 12:34:30 +0530

    Side Project With React

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,46 +1,58 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 // import axios from 'axios';
-import { Route, NavLink } from 'react-router-dom';
+import { Route, NavLink, Switch } from "react-router-dom";
 
-import './Blog.css';
-import Posts from './Posts/Posts';
-import NewPost from './NewPost/NewPost'; 
-import FullPost from './FullPost/FullPost';
+import "./Blog.css";
+import Posts from "./Posts/Posts";
+import NewPost from "./NewPost/NewPost";
+// import FullPost from "./FullPost/FullPost";
 
 class Blog extends Component {
-    render () {
-        return (
-            <div className="Blog">
-                <header>
-                    <nav>
-                        <ul>
-                            <li><NavLink 
-                                to="/" 
-                                exact 
-                                activeClassName="active"
-                                activeStyle={{
-                                    color: '#fa923f',
-                                    textDecoration: 'underline'
-                                }}>Home</NavLink></li>
-                            <li><NavLink to={{
-                                // Relative Path
-                                // pathname: this.props.match.url +  '/new-post',
-                                // Absolute Path
-                                pathname: '/new-post',
-                                hash: '#submit',
-                                search: '?quick-submit=true'
-                            }}>New Post</NavLink></li>
-                        </ul>
-                    </nav>
-                </header>
-                {/* <Route path = "/" exact render={() => <h1>Home</h1>} />
+  render() {
+    return (
+      <div className="Blog">
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <NavLink
+                  to="/posts/"
+                  exact
+                  activeClassName="active"
+                  activeStyle={{
+                    color: "#fa923f",
+                    textDecoration: "underline"
+                  }}
+                >
+                  Posts
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to={{
+                    // Relative Path
+                    // pathname: this.props.match.url +  '/new-post',
+                    // Absolute Path
+                    pathname: "/new-post",
+                    hash: "#submit",
+                    search: "?quick-submit=true"
+                  }}
+                >
+                  New Post
+                </NavLink>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        {/* <Route path = "/" exact render={() => <h1>Home</h1>} />
                 <Route path = "/" render={() => <h1>Home2</h1>} /> */}
-                <Route path="/" exact component={Posts} />
-                <Route path="/new-post" component={NewPost} />
-                <Route path="/:id" exact component={FullPost} />
-            </div>
-        );
-    }
+        <Switch>
+          <Route path="/new-post" component={NewPost} />
+          <Route path="/posts" component={Posts} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default Blog;
