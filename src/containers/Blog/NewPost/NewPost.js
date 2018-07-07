337c4c10db09ed2e9b095bed8a070d4142aefff3
Siddharth Kundu <sk31395@gmail.com>
2018-07-07 20:23:53 +0530

    Side Project Using React

--- src/containers/Blog/NewPost/NewPost.js
+++ src/containers/Blog/NewPost/NewPost.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
+import { Redirect } from "react-router-dom";
 
 import "./NewPost.css";
 
@@ -7,10 +8,12 @@ class NewPost extends Component {
   state = {
     title: "",
     content: "",
-    author: "Max"
+    author: "Max",
+    submitted: false
   };
 
   componentDidMount() {
+    // if unauth => this.props.history.replace('/posts');
     console.log(this.props);
   }
 
@@ -23,12 +26,18 @@ class NewPost extends Component {
 
     axios.post("/posts/", data).then(response => {
       console.log(response);
+      this.props.history.replace("/posts");
     });
   };
 
   render() {
+    let redirect = null;
+    if (this.state.submitted) {
+      redirect = <Redirect to="/posts" />;
+    }
     return (
       <div className="NewPost">
+        {redirect}
         <h1>Add a Post</h1>
         <label>Title</label>
         <input
